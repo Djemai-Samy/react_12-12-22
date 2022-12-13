cd0672f9e5e0737d5eaa3c22a3a9d50ece570200
Samdjm <sam.djm93@gmail.com>
2022-12-13 09:28:48 +0100

    Update README

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import React from "react";
 import Button, { ButtonOutline } from "./Composants/UI/Button/Button";
+
+
 function App() {
 	let nom = "Djemai!";
+
+  function Composant(){
+    return(<div><h1>Hello</h1></div>)
+  }
 
 	return (
 		<div>
 			<h1>{nom}</h1>
 			<p>Hello</p>
 			<p>la suite</p>
+      {Composant()}
+
       <Button color="blue" label="Valider" />
       <ButtonOutline color="blue" label="Valider" />
 		</div>
