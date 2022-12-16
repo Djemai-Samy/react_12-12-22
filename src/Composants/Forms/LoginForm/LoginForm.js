4c8884b75eff6dd9f37c7d714f215bb6b246c6c0
Samdjm <sam.djm93@gmail.com>
2022-12-16 11:22:28 +0100

    login V_2 Pa fini

--- src/Composants/Forms/LoginForm/LoginForm.js
+++ src/Composants/Forms/LoginForm/LoginForm.js
@@ -3,28 +3,20 @@ import Button from "../../UI/Button/Button";
 
 export default function LoginForm() {
 	//1 Créer les variables d'états
-	let [name, setName] = useState("");
-	let [email, setEmail] = useState("");
-	let [password, setPassword] = useState("");
+	let [user, setUser] = useState({ name: "", email: "", password: "" });
 
-	//2 Les fonction handles
-	function handleEmail(e) {
-		setEmail(e.target.value);
-	}
-
-	function handleName(e) {
-		setName(e.target.value);
-	}
-
-	function handlePassword(e) {
-		setPassword(e.target.value);
+	function handleInput(e, nameInput) {
+		//nameInput: "email", "password", "name"
+    setUser({...user, nameInput: e.target.value})
 	}
 
 	//3 La fonction pour valider le formulaire
 	function login() {
 		//Envoyer les donnée a la back end, apres verification des entrées
-		alert(`Email: ${email}. Username: ${name}`);
+		alert(`Email: ${user.email}. Username: ${user.name}`);
 	}
+
+  console.log(user);
 
 	//Lier les composant avec lesvariables et les fonction
 	return (
@@ -32,22 +24,23 @@ export default function LoginForm() {
 			<input
 				type={"text"}
 				placeholder="Entrez votre nom"
-				value={name}
-				onChange={handleName}
+				value={user.name}
+				onChange={(event)=>{ handleInput(event, "name") }}
 			/>
 			<input
 				type={"email"}
 				placeholder="Entrez votre email"
-				value={email}
-				onChange={handleEmail}
+				value={user.email}
+				onChange={(event)=>{ handleInput(event, "email") }}
 			/>
 			<input
 				type="password"
 				placeholder="Mot de passe"
-				value={password}
-				onChange={handlePassword}
+				value={user.password}
+				onChange={(event)=>{ handleInput(event, "password") }}
 			/>
-      <Button title="Login" color="black" fonction={login} />
+			<Button title="Login" color="black" fonction={login} />
 		</div>
 	);
 }
+
